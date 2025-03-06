284fb666f7da0315e3f2c73e0d696f285697d8a0
Tony Aldea <tonythecoder@outlook.com>
2025-03-06 16:01:04 -0500

    Initial commit with project files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './Components/**/*.{razor,html,cs}',
+        './Pages/**/*.{razor,html,cs}',
+        './Layout/**/*.{razor,html,cs}',
+        './wwwroot/index.html',
+        "./node_modules/flowbite/**/*.js"
+    ],
+    plugins: [
+        require('flowbite/plugin')
+    ],
+};
